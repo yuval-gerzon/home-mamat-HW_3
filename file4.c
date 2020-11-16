a001e0998bf699b8335e2079e49f10c9edad03c9
SimonDorr <74558182+SimonDorr@users.noreply.github.com>
2020-11-16 19:24:27 +0200

    Add files via upload

---
+++ file4.c
@@ -0,0 +1,16 @@
+//file4.c
+#include <stdio.h>
+#include <stdlib.h>
+#include "file4.h"
+#include "file0.h"
+#include "file1.h"
+#include "file2.h"
+#include "file3.h"
+int file4_function(){
+	printf("This is file4_function()\n");
+	file0_function();
+	file1_function();
+	file2_function();
+	file3_function();
+	return 0;
+}
